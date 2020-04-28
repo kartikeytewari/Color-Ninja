0271743cb94516a401ae79473de69f623cbce092
Kartikey <kartikeya30@gmail.com>
2020-04-28 20:50:49 +0530

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   index.css
    	new file:   index.html
    	new file:   level_css/easy.css
    	new file:   level_css/medium.css
    	new file:   level_css/tough.css
    	new file:   level_html/easy.html
    	new file:   level_html/medium.html
    	new file:   level_html/tough.html
    	new file:   level_js/easy.js
    	new file:   level_js/medium.js
    	new file:   level_js/tough.js
    	new file:   old/level-1.css
    	new file:   old/level-1.html
    	new file:   old/level-1.js
    
    initiated project skeleton
    old files are kept in folder named "old"

---
+++ old/level-1.js
@@ -0,0 +1,14 @@
+var color_set;
+for (let i=0;i<=5;i++)
+{
+    let a=Math.floor((Math.random()*255)+1);
+    let b=Math.floor((Math.random()*255)+1);
+    let c=Math.floor((Math.random()*255)+1);
+    color_set[i]={a,b,c};
+}
+
+var square=document.querySelectorAll(".square");
+for (let i=0;i<=square.length-1;i++)
+{
+   square[i].style.background="rgb(" + color_set[i].a + ", " + color_set[i].b + ", " + color_set[i].c + ")";
+}
