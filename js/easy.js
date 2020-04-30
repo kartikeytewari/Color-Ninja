0e192c8a967e0859da99bd5415c7b5670f33fd41
Kartikey <kartikeya30@gmail.com>
2020-04-30 14:30:48 +0530

    On branch master  Changes to be committed: 	modified:   js/easy.js 	modified:   js/hard.js 	modified:   js/medium.js
    added some more logic to the code
    so now it "remembers" the choice that you choose
    this clears some bugs that were present in the earlier iteration
    some more bugs have been omitted
    the software as it stands now is in alpha testing
    any bugs or features that would be reported will be changed soon

--- js/easy.js
+++ js/easy.js
@@ -37,6 +37,14 @@ red_bar.style.width=red_display.innerHTML;
 green_bar.style.width=green_display.innerHTML;
 blue_bar.style.width=blue_display.innerHTML;
 
+
+// the indexed that have been clicked
+let clicked=[];
+for (let i=0;i<=square.length-1;i++)
+{
+    clicked[i]=0;
+}
+
 // logic of game
 var score=5;
 attempt_display.innerHTML=0;
@@ -44,43 +52,48 @@ for (let i=0;i<=square.length-1;i++)
 {
     square[i].addEventListener("click", function()
     {
-        if (i==ans)
+        if (clicked[i]===0)
         {
-            // correct answer
-            i=6;
-            if (score==0)
+            clicked[clicked.length]=i;
+            if (i==ans)
             {
-                window.location.replace("score_0.html");
+                // correct answer
+                i=6;
+                if (score==0)
+                {
+                    window.location.replace("score_0.html");
+                }
+                else if (score==1)
+                {
+                    window.location.replace("score_1.html");
+                }
+                else if (score==2)
+                {
+                    window.location.replace("score_2.html");
+                }
+                else if (score==3)
+                {
+                    window.location.replace("score_3.html");
+                }
+                else if (score==4)
+                {
+                    window.location.replace("score_4.html");
+                }
+                else if (score==5)
+                {
+                    window.location.replace("score_5.html");
+                }
             }
-            else if (score==1)
+            else
             {
-                window.location.replace("score_1.html");
+                // wrong answer
+                square[i].style.background="#232323";
+                score--;
             }
-            else if (score==2)
-            {
-                window.location.replace("score_2.html");
-            }
-            else if (score==3)
-            {
-                window.location.replace("score_3.html");
-            }
-            else if (score==4)
-            {
-                window.location.replace("score_4.html");
-            }
-            else if (score==5)
-            {
-                window.location.replace("score_5.html");
-            }
-        }
-        else
-        {
-            // wrong answer
-            square[i].style.background="#232323";
-            score--;
         }
 
-        attempt_display.innerHTML=i+1;
+        clicked[i]=1;
+        attempt_display.innerHTML=5-score;
     })
 }
 
